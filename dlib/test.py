92ca13086e97bde4578954fae91af85fe6ec1254
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-08-30 23:06:39 +0530

    Test.py Updated with image resizing and graph plotting & added point.txt file which has co-ordinates of points

--- dlib/test.py
+++ dlib/test.py
@@ -47,6 +47,12 @@ import os
 import dlib
 import glob
 from skimage import io
+import PIL
+from PIL import Image
+from resizeimage import resizeimage
+import matplotlib.pyplot as plt
+
+
 
 if len(sys.argv) != 3:
     print(
@@ -60,35 +66,56 @@ if len(sys.argv) != 3:
     exit()
 
 predictor_path = sys.argv[1]
-faces_folder_path = sys.argv[2]
-
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(predictor_path)
 win = dlib.image_window()
 
-for f in glob.glob(os.path.join(faces_folder_path, "*.jpg")):
-    print("Processing file: {}".format(f))
-    img = io.imread(f)
 
-    win.clear_overlay()
-    win.set_image(img)
+orig_stdout = sys.stdout
+fi = open('point.txt','w')
+sys.stdout = fi
+
+basewidth = 700
+with open(sys.argv[2],'r+b') as f:
+	with Image.open(f) as image:
+		 cover = resizeimage.resize_cover(image, [700, 700])
+       		 cover.save(sys.argv[2], image.format)
+
+
+img = io.imread(sys.argv[2])
+
+
+
+win.clear_overlay()
+win.set_image(img)
 
     # Ask the detector to find the bounding boxes of each face. The 1 in the
     # second argument indicates that we should upsample the image 1 time. This
     # will make everything bigger and allow us to detect more faces.
-    dets = detector(img, 1)
-    print("Number of faces detected: {}".format(len(dets)))
-    for k, d in enumerate(dets):
-        print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
-            k, d.left(), d.top(), d.right(), d.bottom()))
+dets = detector(img, 1)
+for k, d in enumerate(dets):
         # Get the landmarks/parts for the face in box d.
-        shape = predictor(img, d)
-        for i in xrange(68):
-		print "Part 0:",shape.part(i)
-	print("Part 0: {}, Part 1: {} ...".format(shape.part(0),
-                                                  shape.part(1)))
-        # Draw the face landmarks on the screen.
-        win.add_overlay(shape)
+	#print shape.part(1),shape.part(2)
+	shape = predictor(img, d)
+	for i in xrange(68):
+		print shape.part(i)
 
-    win.add_overlay(dets)
-    dlib.hit_enter_to_continue()
+	sys.stdout = orig_stdout
+	fi.close()
+
+	fi = open('point.txt', 'r')
+	for line in fi:
+		x1 = line[1]+line[2]+ line[3]
+		y1 = line[5] + line[6] + line[7]
+		x1 = int(x1)
+		y1 = int(y1)
+		plt.plot(x1,y1,'bo')
+		print x1,y1
+
+	plt.show()
+		
+        # Draw the face landmarks on the screen.
+	win.add_overlay(shape)
+
+win.add_overlay(dets)
+dlib.hit_enter_to_continue()
