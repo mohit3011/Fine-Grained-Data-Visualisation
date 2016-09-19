11bce8b7bf5544fb1c5f5950db57d12d1ba5d719
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-09-19 22:42:55 +0530

    Added testcorner.py which detects major corner points in the image

---
+++ dlib/testcorner.py
@@ -0,0 +1,24 @@
+import cv2
+import numpy as np
+
+
+filename = raw_input("Enter the Image Filename: ")
+filename  = str(filename)
+
+
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+
+cv2.imshow('dst',img)
+k = cv2.waitKey(15000)
+if k==27 or k==-1:
+    cv2.destroyAllWindows()
