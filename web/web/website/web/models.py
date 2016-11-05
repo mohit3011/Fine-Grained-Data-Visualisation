42b16777f05a0917cc6ea9bdcf04bcf19f2d67a1
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-11-05 22:43:15 +0530

    added features for integration of modules

--- web/web/website/web/models.py
+++ web/web/website/web/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+	content = models.FileField(null=True, blank=True)
+
+	def __unicode__(self):
+		return self.content
+
+	def __str__(self):
+		return self.title
