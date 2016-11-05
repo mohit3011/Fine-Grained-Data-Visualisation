862fcca448ee420d5dd6a325087576907e55b4a8
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-11-05 22:40:10 +0530

    Changed views.py for integrating the modules

--- web/web/website/web/views.py
+++ web/web/website/web/views.py
@@ -1,8 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect,Http404
 from django.core.urlresolvers import reverse
 from django.contrib import messages
-
+from .models import Post
+from .forms import PostForm
 
 
 
@@ -11,5 +17,18 @@ def index(request):
 	context = {"hello":stri
 	   }
 	return render(request, "web/index.html", context)
+
+
+def uploadfile(request):
+
+	form = PostForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		instance = form.save(commit=False)
+		instance.save()
+		messages.success(request, "Successfully Created")
+
+	context = {"form":form}
+
+	return render(request,"web/uploadfile.html",context)
 
 
