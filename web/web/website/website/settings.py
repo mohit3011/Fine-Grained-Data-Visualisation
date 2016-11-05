8f099ea429809257e1704739b06ef0893620af4a
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-11-05 22:41:15 +0530

    changed settings.py and urls.py

--- web/web/website/website/settings.py
+++ web/web/website/website/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'crispy_forms',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES= [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,6 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 
 
 AUTHENTICATION_BACKENDS = (
--- web/web/website/website/urls.py
+++ web/web/website/website/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -24,3 +26,7 @@ urlpatterns = [
    
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL , document_root=settings.MEDIA_ROOT)
