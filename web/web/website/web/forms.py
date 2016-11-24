8af1fa2d2e6eb91ed949cd6db4556b55c8009ec2
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-11-25 01:53:39 +0530

    added form for Twitter

--- web/web/website/web/forms.py
+++ web/web/website/web/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Post
+from .models import Post1
+
 
 class PostForm(forms.ModelForm):
 	class Meta:
 		model = Post
 		fields = ["content",]
+
+class PostForm1(forms.ModelForm):
+	class Meta:
+		model = Post1
+		fields = ["hash1","hash2","hash3",]
