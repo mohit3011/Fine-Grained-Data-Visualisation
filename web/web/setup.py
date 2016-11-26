565cbc89734e14c0187484ca9d8b2b354360ab27
mohit3011 <mohit.chandra@research.iiit.ac.in>
2016-11-26 22:22:25 +0530

    Added setup.py

---
+++ web/web/setup.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('sudo pip install django-allauth')
+os.system('sudo pip install django-crispy-forms')
+os.system('sudo pip install matplotlib')
+os.system('sudo pip install pandas')
